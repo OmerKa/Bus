fa8342c4f7e1a07a544a7bf846559304420e5cb8
Omer <omer@home>
2012-07-20 19:36:44 +0300

    Add db licary to connect to db

---
+++ db.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+conn = sqlite3.connect('./DB/tranz.sqlite')
+
+def Query(sql):
+    c = conn.cursor()
+    c.execute(sql)
+    l = c.fetchall()
+    c.close()
+    return l
+
+def First(l):
+     return [r[0] for r in l]
+
+def get_trips_ids(col,l):
+    sql = 'select distinct id from trips where {0} in {1}'.format(col,tuple(l))
+    return First(Query(sql))
